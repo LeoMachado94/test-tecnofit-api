56de49aa1ad88a3302595acc86e653a04345756d
LeoMachado94 <leo94.machado@gmail.com>
2022-02-19 22:44:42 -0300

    Models Created

---
+++ app/Models/PersonalRecord.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class PersonalRecord extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'movement_id',
+        'value',
+        'date',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function movement(): BelongsTo
+    {
+        return $this->belongsTo(Movement::class);
+    }
+}
