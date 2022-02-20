8efe50e200b790511c0a478227304cf3f27f361d
LeoMachado94 <leo94.machado@gmail.com>
2022-02-20 03:49:22 -0300

    Created Controller Code

--- app/Http/Controllers/Api/Ranking/RankingController.php
+++ app/Http/Controllers/Api/Ranking/RankingController.php
@@ -3,8 +3,55 @@
 namespace App\Http\Controllers\Api\Ranking;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movement;
+use App\Models\PersonalRecord;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RankingController extends Controller
 {
-    //
+    public function index(Request $request, Movement $movement)
+    {
+        $ranking = PersonalRecord::query()
+            ->join('users as u', 'u.id', '=', 'personal_records.user_id')
+            ->select([
+                'u.name',
+                DB::raw('MAX(personal_records.value) as score'),
+            ])
+            ->where('personal_records.movement_id', $movement->id)
+            ->groupBy('personal_records.user_id')
+            ->orderBy('score', 'DESC')
+            ->get()
+            ->groupBy('score')
+            ->values()
+            ->toArray();
+
+        return response()->json([
+            'success' => true,
+            'data' => $ranking
+        ]);
+    }
+
+    public function general(Request $request)
+    {
+        $ranking = PersonalRecord::query()
+            ->join('users as u', 'u.id', '=', 'personal_records.user_id')
+            ->select([
+                'u.name',
+                DB::raw('SUM(personal_records.value) as score'),
+            ])
+            ->groupBy('personal_records.user_id')
+            ->orderBy('score', 'DESC')
+            ->get()
+            ->groupBy('score')
+            ->values()
+            ->toArray();
+
+        return response()->json([
+            'success' => true,
+            'data' => $ranking
+        ]);
+    }
+
+
 }
